bcebaa5fa85da190d7918fea911f55098ebb822b
Jacob Meline <jmeline@users.noreply.github.com>
2024-09-27 22:19:26 -0600

    Add hello world

---
+++ 01_hello_world/src/main.rs
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------
+// Title: Hello, world!
+// Description: The classic first program.
+// -----------------------------------------------------------------
+// Objective:
+// 1. Print "Hello, world!" to the console.
+// 2. Print "I'm a Rustacean!" to the console.
+// -----------------------------------------------------------------
+// Usage:
+// 1. Run the program.
+// -----------------------------------------------------------------
+// Example:
+// $ cargo run
+// -----------------------------------------------------------------
+// Notes:
+// 1. The println! macro prints text to the console.
+// 2. The ! indicates that println! is a macro.
+// 3. The println! macro is used to print text to the console.
+
+fn main() {
+    println!("Hello, world!");
+    println!("I'm a Rustacean!");
+}
